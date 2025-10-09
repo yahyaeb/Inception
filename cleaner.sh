6a27c8485653ae6c843ca3b1e5f2e2a5a75a68d7
Lepokile <yahya.elboukili1@gmail.com>
2025-10-09 09:57:11 -0400

    working version v2

---
+++ cleaner.sh
@@ -0,0 +1,1 @@
+sudo docker stop $(sudo docker ps -qa); sudo docker rm $(sudo docker ps -qa); sudo docker rmi -f $(sudo docker images -qa); sudo docker volume rm $(sudo docker volume ls -q); sudo docker network rm $(sudo docker network ls -q) 2>/dev/null
